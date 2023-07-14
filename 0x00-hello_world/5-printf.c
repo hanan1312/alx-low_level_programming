78f2dcb3aa3b26e9d3a1a8c353f4b4802756619b
hanan1312 <nana.elmogey@gmail.com>
2023-07-14 02:26:08 +0200

    my first C code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ /*
+ * main function to return a statement
+ * Return:
+ *   None
+ */
+int main(void)
+ {
+    printf("With proper grammar, but the outcome is a piece of art,\n");
+    return 0;}
