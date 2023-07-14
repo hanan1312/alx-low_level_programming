94c3e0dd1a06e54ed2622caaec681889b6b745d4
hanan1312 <nana.elmogey@gmail.com>
2023-07-14 02:19:52 +0200

    my first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+ * Prints the message "Programming is like building a multilingual puzzle" to the console.
+ * Return:
+ *   0
+ */
 int main(void)
  {
     puts("Programming is like building a multilingual puzzle");
