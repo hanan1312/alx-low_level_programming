cf2eacd6fa8eef5e2a3075a9966cd725cb059dc5
hanan1312 <nana.elmogey@gmail.com>
2023-07-15 00:19:04 +0200

    my first C code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
