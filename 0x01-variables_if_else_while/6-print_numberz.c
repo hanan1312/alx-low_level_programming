d4b951e4fa34ef16ac1c75335467b2ef8b756160
hanan1312 <nana.elmogey@gmail.com>
2023-07-15 02:12:25 +0200

    my first C code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+{
+		putchar('0' + count);
+		count++;
+}
+	putchar('\n');
+	return (0);
+}
