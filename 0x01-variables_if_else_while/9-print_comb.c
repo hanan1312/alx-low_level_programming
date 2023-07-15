ac767f29ed625d7c8d74814ff5736c5138f8bc3b
hanan1312 <nana.elmogey@gmail.com>
2023-07-15 02:14:28 +0200

    my first C code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+{
+	putchar('0' + i);
+	if (i < 9)
+{
+		putchar(',');
+		putchar(' ');
+}
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
