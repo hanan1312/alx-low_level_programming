fcfee6d6f78777bd4fdc9f5132c1e5ad630ea921
hanan1312 <nana.elmogey@gmail.com>
2023-07-16 02:19:01 +0200

    advanced commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,13 @@
 int main(void)
 {
 	int i, j;
+	
 	for (i = 0; i < 9; i++)
 {
 	for (j = i + 1; j <= 9; j++)
 {
 	int num = (i * 10) + j;
+	
 	putchar((num / 10) + '0');
 	putchar((num % 10) + '0');
 	if (i != 8 || j != 9)
