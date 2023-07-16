a2fb71d1c13a12dfbd3da1ef906ab228c09474ee
hanan1312 <nana.elmogey@gmail.com>
2023-07-16 02:14:03 +0200

    advanced commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+	for (i = 0; i < 9; i++)
+{
+	for (j = i + 1; j <= 9; j++)
+{
+	int num = (i * 10) + j;
+	putchar((num / 10) + '0');
+	putchar((num % 10) + '0');
+	if (i != 8 || j != 9)
+{
+	putchar(',');
+	putchar(' ');
+ }
+ }
+ }
+	return (0);
+}
