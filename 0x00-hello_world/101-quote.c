9caaa4125fcd8c473dbf60832de647d08c6c6424
hanan1312 <nana.elmogey@gmail.com>
2023-07-16 02:03:49 +0200

    advanced commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main - int  Entry
-*Description: 'print a quote'
-*Return: 0 
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" -
-\Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
 	return (0);
 }
