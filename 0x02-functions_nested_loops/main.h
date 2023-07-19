1817480abf39945cefc73e0329e917451ee7113f
hanan1312 <nana.elmogey@gmail.com>
2023-07-19 23:17:33 +0200

    advanced commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <stdlib.h>
+/**
+*print_alphabet - helper function
+*Description: 'print string'
+*@c: charecter to start
+*@i: counter
+*Return: Always 0 (Success)
+*/
+int _putchar(char c);
