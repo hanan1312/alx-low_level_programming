dd580a223ce9c1d695a1618a31aaeda5d72595ab
hanan1312 <nana.elmogey@gmail.com>
2023-07-19 02:45:34 +0200

    advanced commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*main - Entry point
+*Description: 'print string'
+*@c: charecter printed
+*Return: Always 0 (Success)
+*/
+int _putchar(char c);
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
