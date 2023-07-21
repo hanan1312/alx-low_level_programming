812de6acbafb7e7cb35ab2dd408d5eae2b72623f
hanan1312 <nana.elmogey@gmail.com>
2023-07-21 03:42:39 +0200

    advanced commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+/**
+*_isupper - helper function
+*Description: 'print string'
+*@c: input charecter
+*Return: Always 0 (Success)
+*/
+int _isdigit(int c)
+{
+	int i;
+
+	if (_isdigit(c))
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+	return (i);
+}
