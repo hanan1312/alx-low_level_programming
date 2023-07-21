1821c24cdbe3b9bdf1d29e20283f23c4b88f250b
hanan1312 <nana.elmogey@gmail.com>
2023-07-21 03:13:18 +0200

    advanced commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,21 +6,22 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && b > c)
-{
-largest = a;
-}
-else if (b > a && a > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
