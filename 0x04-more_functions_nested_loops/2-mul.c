1743ddacf5d61a398ebe1f37483cfec915e5b525
hanan1312 <nana.elmogey@gmail.com>
2023-07-21 03:55:18 +0200

    advanced commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+*mul - helper function
+*Description: 'print string'
+*@a: first input number
+*@b: second input number
+*Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
