358d90bf403d4501ca1552abada224a364b15e40
hanan1312 <hanan1312@gmail.com>
2023-07-25 02:32:13 +0200

    last_c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
