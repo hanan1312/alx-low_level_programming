275b89072e433a2d45c361f2d853d0843d9d97f7
hanan1312 <hanan1312@gmail.com>
2023-07-25 03:37:36 +0200

    last_c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,8 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d", a[index]);
-		if (index != (n - 1))
+		printf("%d", a[i]);
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
