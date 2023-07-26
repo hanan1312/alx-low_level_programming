0bc486524ceed9f9f1f37d9eb6873be58ca5c070
hanan1312 <hanan1312@gmail.com>
2023-07-27 01:29:29 +0200

    another_c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,29 +5,29 @@
  * @str: input
  * Return: Always 0.
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int i, j;
 
 	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == spe[j])
+			if (str[i] == spe[j])
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
-					s[i + 1] -= 32;
+					str[i + 1] -= 32;
 				}
 			}
 		}
 	}
 
-	return (s);
+	return (str);
 }
