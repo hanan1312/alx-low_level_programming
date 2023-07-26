d7fe5a2a25d6b16f9857ff35aaf469b746f9497f
hanan1312 <hanan1312@gmail.com>
2023-07-27 01:26:16 +0200

    another_c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,14 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; ++i)
-	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '.' || str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == '?')
-		{
-		str[i] = toupper(str[i]);
-		}
-	}
-	return (str);
+for (i = 0; str[i] != '\0'; ++i)
+{
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '.' || str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == '?')
+{
+str[i] = toupper(str[i]);
+}
+}
+return (str);
 }
