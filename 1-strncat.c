ea2ecca4191d52a89a7db49cc318f3f41c429392
hanan1312 <hanan1312@gmail.com>
2023-07-27 01:07:03 +0200

    another_c

---
+++ 1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
