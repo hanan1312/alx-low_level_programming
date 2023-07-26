11b9371652af8dd7a767c32c49f9de2a3a84ae0c
hanan1312 <hanan1312@gmail.com>
2023-07-27 01:48:12 +0200

    another_c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,19 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int new_n;
 
-	n1 = n;
+	 new_n = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		new_n = -n;
 	}
 
-	if (n1 / 10 != 0)
+	if (new_n / 10 != 0)
 	{
-		print_number(n1 / 10);
+		print_number(new_n / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar((new_n % 10) + '0');
 }
