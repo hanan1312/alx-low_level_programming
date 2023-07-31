c8838572196578a69455a6505bc0c9167087afe6
hanan1312 <hanan1312@gmail.com>
2023-08-01 00:17:19 +0200

    another_c

---
+++ 1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
