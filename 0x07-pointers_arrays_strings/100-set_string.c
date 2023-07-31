d7093a20236f8899a4956e23be9a78cf0a8313ad
hanan1312 <hanan1312@gmail.com>
2023-08-01 00:48:40 +0200

    another_c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strspn - function that sets the value of a pointer to a char.
+ * set_string - function that sets the value of a pointer to a char.
  * @s: target location
- * @to_str: string to set
+ * @to: string to set
  * Return: charecter.
  */
-void set_string(char **s, char *to_str)
+void set_string(char **s, char *to)
 {
-	*s = to_str;
+	*s = to;
 }
