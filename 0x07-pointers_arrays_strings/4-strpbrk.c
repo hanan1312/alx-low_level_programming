d84d099a199d1d854507da53e8eab4134fd6fb5c
hanan1312 <hanan1312@gmail.com>
2023-08-01 00:30:02 +0200

    another_c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+#include <string.h>
 /**
- * _strpbrk - searches a string for any of a set of bytes
+ * _strpbrk - check the code
  * @s: target location
- * @accept:value
- * Return: character
+ * @accept: value
+ * Return: charecter.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	return (strpbrk(s, accept));
+return (strpbrk(s, accept));
 }
