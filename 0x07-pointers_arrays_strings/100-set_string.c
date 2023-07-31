54915fc940980830e1c3a181008b40957fab99b0
hanan1312 <hanan1312@gmail.com>
2023-08-01 00:45:11 +0200

    another_c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strspn - function that sets the value of a pointer to a char.
+ * @s: target location
+ * @accept: value
+ * Return: charecter.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
