b4ba6824ecc6233a0f936229ed7b797e59010d0c
hanan1312 <hanan1312@gmail.com>
2023-08-01 00:28:29 +0200

    another_c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,9 +3,9 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: string to search
- * @accept: stringcontaining the bytes to look for
- * Return: pointer to the byte in s that matches one of the bytes in accept
+ * @s: target location
+ * @accept:value
+ * Return: character
  */
 char *_strpbrk(char *s, char *accept)
 {
