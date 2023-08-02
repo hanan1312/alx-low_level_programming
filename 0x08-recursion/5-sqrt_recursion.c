8c13e5db9f84becf6dd563b914d249c5f23dd029
hanan1312 <hanan1312@gmail.com>
2023-08-03 01:58:46 +0200

    another_c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+/**
+ * helper_fn - check the code
+ * @n: input number
+ * @i: number
+ * Return: charecter.
+ */
+int aid_func(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+return (aid_func(n, i + 1));
+}
+/**
+ * _sqrt_recursion - check the code
+ * @n: input number
+ * Return: charecter.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (aid_func(n, 0));
+	}
+}
