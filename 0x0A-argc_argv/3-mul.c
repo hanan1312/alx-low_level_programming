8652b2121b45b4d764d6494570e72cb59269e122
hanan1312 <hanan1312@gmail.com>
2023-08-05 02:09:43 +0200

    another_c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
 *main - start
 *Description: 'print string'
@@ -10,16 +9,15 @@
 */
 int main(int argc, char *argv[])
 {
-	int frst_ n;
-	int scnd_ n;
-
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	frst_ n = atoi(argv[1]);
-	scnd_ n = atoi(argv[2]);
-	printf("%d\n", frst_ n *scnd_ n);
-	return (0);
+int n1;
+int n2;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 *n2);
+return (0);
 }
