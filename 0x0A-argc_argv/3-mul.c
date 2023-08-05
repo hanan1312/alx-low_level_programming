10e3274c8336e18d1f8f2479d10f617549633320
hanan1312 <hanan1312@gmail.com>
2023-08-05 02:08:41 +0200

    another_c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,14 @@
 */
 int main(int argc, char *argv[])
 {
-int frst_ n;
-int scnd_ n;
-if (argc < 3)
-{
-	printf("Error\n");
-	return (1);
-}
+	int frst_ n;
+	int scnd_ n;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	frst_ n = atoi(argv[1]);
 	scnd_ n = atoi(argv[2]);
 	printf("%d\n", frst_ n *scnd_ n);
