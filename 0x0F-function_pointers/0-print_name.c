628372a5605075fabd2705906c8fb880a522efb5
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-17 03:13:21 +0300

    processor

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
+#include "function_pointers.h"
+
 /**
-*print_name - prints a name
-*Description: 'print string'
-*@name: name
-*@f: function
-*Return: void
-*/
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
-{
-return;
-}
-f(name);
-return;
+	if (name && f)
+		f(name);
 }
