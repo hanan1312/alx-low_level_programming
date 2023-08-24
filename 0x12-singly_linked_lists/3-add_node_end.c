7f585a6ac19336368ae2281a360b340707e6d3c6
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-25 01:59:44 +0300

    linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
-	size_t nchar;
+	size_t n_char;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -17,10 +17,10 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++)
+	for (n_char = 0; str[n_char]; n_char++)
 		;
 
-	new->len = nchar;
+	new->len = n_char;
 	new->next = NULL;
 	temp = *head;
 
