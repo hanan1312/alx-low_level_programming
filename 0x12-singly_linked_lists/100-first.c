f843de0d4c68b344b6a9260825cf75d4a2f2158d
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-25 02:08:39 +0300

    linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+
+#include "lists.h"
+/**
+ * start - start
+ * Return: nothing.
+ */
+
+void start(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf("\nI bore my house upon my back!\n");
+}
