f9beb6c008719f2110c769ceb1af27a203b52b1c
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-30 04:47:01 +0300

    still singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,30 +9,30 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
   /* Allocate memory for the new node */
-	listint_t *new_node;
-	istint_t *last_node = *head;
+listint_t *new_node;
+istint_t *last_node = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
 
   /* Set the data of the new node */
-	new_node->n = n;
+new_node->n = n;
   /* Initialize the next pointer of the new node */
-	new_node->next = NULL;
+new_node->next = NULL;
   /* If the list is empty, make the new node the head */
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
   /* Otherwise, find the last node and add the new node after it */
-	while (last_node->next)
-	{
-		last_node = last_node->next;
-	}
-	last_node->next = new_node;
-	return (new_node);
+while (last_node->next)
+{
+last_node = last_node->next;
+}
+last_node->next = new_node;
+return (new_node);
 }
