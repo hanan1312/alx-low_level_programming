16ce428aa941fdf96b790ed435f9ebab404c3fa5
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-30 04:25:31 +0300

    still singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,12 +17,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
   }
 
   /* Set the data of the new node */
-	new_node->data = n;
+	new_node->n = n;
 
   /* Set the next pointer of the new node to the current head */
 	new_node->next = *head;
 
   /* Make the new node the new head */
 	*head = new_node;
-	return new_node;
+	return (new_node);
 }
