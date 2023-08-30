282a39d08a5761fae47ddfd52260b32965fd359d
Hanan Elmogey <nana.elmogey@gmail.com>
2023-08-30 04:18:50 +0300

    still singly linked lists

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+*add_nodeint - start
+*Description: 'print string'
+*@head: sep
+*@n: int
+*Return: void
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+  /* Allocate memory for the new node */
+  listint_t *new_node = malloc(sizeof(listint_t));
+  if (new_node == NULL) {
+    return NULL;
+  }
+
+  /* Set the data of the new node */
+  new_node->data = n;
+
+  /* Set the next pointer of the new node to the current head */
+  new_node->next = *head;
+
+  /* Make the new node the new head */
+  *head = new_node;
+
+  return new_node;
+}
