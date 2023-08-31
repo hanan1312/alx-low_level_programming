c0f632aeef0687e1bc035fa7dee939acbae93228
Hanan Elmogey <nana.elmogey@gmail.com>
2023-09-01 00:11:07 +0300

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary representation of a number
+ * @n:number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	int j;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (i = n, j = 0; (i >>= 1) > 0; j++)
+		;
+
+	for (; j >= 0; j--)
+	{
+		if ((n >> j) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
