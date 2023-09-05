ba89efa90ca11147b05bead879cc4841856c3d71
Hanan Elmogey <nana.elmogey@gmail.com>
2023-09-05 03:22:19 +0300

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open, write, i = 0;
+	int op, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 			i++;
 	}
 
-	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(open, text_content, i);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = wr(op, text_content, i);
 
-	if (open == -1 || write == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
-	close(open);
+	close(op);
 
 	return (1);
 }
