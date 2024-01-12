a30279bd1d1048b1af89169048fe1abaa335bef8
h1312 <nana.elmogey@gmail.com>
2024-01-12 02:07:34 +0200

    linked

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *print_dlistint - doubly linked list
+ *@h: input
+ *Description: doubly linked list node structure
+ *Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL) {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
+
+    return (count);
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ my first C script
 first C
 debugging
 0x09-static_libraries
+0x17-doubly_linked_lists
