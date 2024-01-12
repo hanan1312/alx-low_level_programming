e45c0658779000faecbbe79f76ac734364e3e631
h1312 <nana.elmogey@gmail.com>
2024-01-12 02:13:40 +0200

    linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,26 @@
 #include "lists.h"
 /**
- *print_dlistint - doubly linked list len
+ *dlistint_len - doubly linked list
  *@h: input
- *Return: count
+ *Description: doubly linked list node structure
+ *Return: size_t
  */
- size_t dlistint_len(const dlistint_t *h)
- {
-    size_t count = 0;
+size_t dlistint_len(const dlistint_t *h)
+{
+int node_count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return (count);
+if (h == NULL)
+{
+return (node_count);
+}
+while (h->prev != NULL)
+{
+h = h->prev;
+}
+while (h != NULL)
+{
+node_count++;
+h = h->next;
+}
+return (node_count);
 }
