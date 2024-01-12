2370ccfdce7e9565c0627fcaeae591757650e89e
h1312 <nana.elmogey@gmail.com>
2024-01-12 02:43:58 +0200

    linked

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,29 +17,32 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-/**
- *add_dnodeint - doubly linked list
- *@head: input
- *@n: input
- *Description: doubly linked list node structure
- *Return: dlistint_t
- */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	dlistint_t *h;
 
-if (new_node == NULL)
-{
-return (NULL);
-}
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = *head;
-if (*head != NULL)
-{
-(*head)->prev = new_node;
-}
-*head = new_node;
-return (new_node);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+
+	return (new);
 }
 
