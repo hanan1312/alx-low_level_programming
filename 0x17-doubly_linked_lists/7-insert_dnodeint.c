8ec5ef9c701db8ed87f9087afc0ca45061c21687
h1312 <nana.elmogey@gmail.com>
2024-01-12 03:19:01 +0200

    linked

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,56 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a dlistint_t linked list.
- * @h: Pointer to a pointer to the head node of the linked list.
- * @idx: Index at which the new node should be inserted.
- * @n: Value to be stored in the new node.
- * Return: Pointer to the newly inserted node, or NULL on failure.
+ *add_dnodeint_end - doubly linked list
+ *@head: input
+ *@n: input
+ *Description: doubly linked list node structure
+ *Return: size_t
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    if (h == NULL)
-        return NULL;
+dlistint_t *current, *new = malloc(sizeof(dlistint_t));
+unsigned int i;
 
-    dlistint_t *new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return NULL;
-
-    new->n = n;
-    new->prev = NULL;
-    new->next = NULL;
-
-    if (idx == 0)
-    {
-        new->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new;
-        *h = new;
-        return new;
-    }
-
-    dlistint_t *current = *h;
-    unsigned int i = 0;
-
-    while (i < idx - 1 && current != NULL)
-    {
-        current = current->next;
-        i++;
-    }
-
-    if (current == NULL)
-    {
-        free(new);
-        return NULL;
-    }
-
-    new->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = new_node;
-    current->next = new_node;
-    new->prev = current;
-
-    return (new);
+if (h == NULL)
+return (NULL);
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+new->prev = NULL;
+new->next = NULL;
+if (idx == 0)
+{
+new->next = *h;
+if (*h != NULL)
+{
+(*h)->prev = new;
+*h = new;
+return (new);
+}
+}
+current = *h;
+for (i = 0; i < idx - 1; i++)
+{
+if (current == NULL)
+{
+free(new);
+return (NULL);
+}
+current = current->next;
+}
+if (current == NULL)
+{
+free(new);
+return (NULL);
+}
+new->prev = current;
+new->next = current->next;
+if (current->next != NULL)
+{
+current->next->prev = new;
+}
+current->next = new;
+return (new);
 }
