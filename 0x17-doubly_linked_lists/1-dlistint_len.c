78db042f9b014747b6333e31eae8e9a0e0c72548
h1312 <nana.elmogey@gmail.com>
2024-01-12 02:42:25 +0200

    linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,20 +7,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-int node_count = 0;
+	int count;
 
-if (h == NULL)
-{
-return (node_count);
-}
-while (h->prev != NULL)
-{
-h = h->prev;
-}
-while (h != NULL)
-{
-node_count++;
-h = h->next;
-}
-return (node_count);
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
